2d17453b54477846415fd3b87ae3aa8468f0e367
tkeremc <tkeremc@outlook.com>
2024-11-27 17:42:42 +0300

    init

---
+++ bank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;using System.Runtime.InteropServices.JavaScript;
+using System.Threading.Channels;
+
+
+namespace bank
+{
+    public class Startup
+    { 
+        public Startup()
+        {
+            Console.WriteLine("iBank'a hoş geldiniz!");
+            Thread.Sleep(1500);
+            Console.Clear();
+        }
+    
+    }
+
+    public class User()
+    {
+        
+    }
+
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+        Startup startup = new Startup();
+        
+        }
+    }
+}
